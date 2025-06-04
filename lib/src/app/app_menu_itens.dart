5f54ac3026228d6ba487241e162c67c47e5e37db
= <soaresrodrigobender@gmail.com>
2025-06-04 11:31:12 -0300

    commit inicial

---
+++ lib/src/app/app_menu_itens.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:utilidades/src/models/menu_models.dart';
+import 'package:utilidades/src/views/home_view.dart';
+
+final List<MenuModels> appMenuItemns = [
+  MenuModels(title: "Nome", icon: Icons.home, route: "/home", page: const HomeView()),
+];
